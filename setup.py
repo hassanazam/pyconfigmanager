df531289927f7f575e2630258f85ddb5d50844f7
HassanAzam <hazam@foree.co>
2020-08-07 23:20:40 +0500

    Updated setup.cfg.

--- setup.py
+++ setup.py
@@ -3,18 +3,16 @@ from setuptools import setup
 setup(
   name = 'pyconfigmanager',
   packages = ['pyconfigmanager'],
-  version = '0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'A simple config manager to load configuration file from outside of project',
   author = 'MHA',
   author_email = 'hassanazam@live.com',
   url = 'https://github.com/hassanazam/pyconfigmanager',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  download_url = 'https://github.com/HassanAzam/pyconfigmanager/archive/0.0.1.tar.gz',
   keywords = ['python config manager', 'config management', 'python config'],
   install_requires=[
-          'validators',
-          'beautifulsoup4',
-      ],
+  ],
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
