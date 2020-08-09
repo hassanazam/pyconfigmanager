cc17804799fd2694e69e0daf0127d19658cb4a10
HassanAzam <hazam@foree.co>
2020-08-10 00:00:26 +0500

    Updated long_description.

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
   name = 'pyconfigmanager',
   packages = ['pyconfigmanager'],
-  version = 'v0.2.3-alpha',
+  version = 'v0.2.4-alpha',
   license='MIT',
   description = 'A simple config manager to load configuration file from outside of project',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'MHA',
   author_email = 'hassanazam@live.com',
   url = 'https://github.com/hassanazam/pyconfigmanager',
-  download_url = 'https://github.com/HassanAzam/pyconfigmanager/archive/v0.2.3-alpha.tar.gz',
+  download_url = 'https://github.com/HassanAzam/pyconfigmanager/archive/v0.2.4-alpha.tar.gz',
   keywords = ['python config manager', 'config management', 'python config'],
   install_requires=[
   ],
